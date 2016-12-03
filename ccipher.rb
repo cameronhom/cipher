7189cb51c497e685b09659fcc4586481915a1b19
Cameron <cameron@CC.attlocal.net>
2016-12-03 01:39:02 -0800

    odin cipher

---
+++ ccipher.rb
@@ -0,0 +1,15 @@
+#ccipher
+
+def cipher (word, set)
+	# newword = ""
+	# word.each_char do |i|
+	# 	if(i == "a")
+	# 		i = "z"
+	# 		next
+
+end
+
+x = ("a".."z").to_a
+z = "abcdefghijklmnopqrstuvwxyz"
+
+puts z[0+2]
